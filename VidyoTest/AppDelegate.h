90b40b896c37d236e6b9c6a72cf680c820bfb482
11 Health and Technologies <MB2@11s-MacBook-Pro.local>
2017-11-21 11:10:06 -0800

    Initial Commit

---
+++ VidyoTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VidyoTest
+//
+//  Created by 11 Health and Technologies on 11/21/17.
+//  Copyright © 2017 11 Health and Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VidyoTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VidyoTest
+//
+//  Created by 11 Health and Technologies on 11/21/17.
+//  Copyright © 2017 11 Health and Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
